6d765bacef5ab264b43b9843b687385bc9cdaed5
Tan-Furukawa <furukawatan@gmail.com>
2024-09-02 15:17:34 +0900

    sync zenode

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coLamB",
-    version="0.1.1",
+    version="0.1.2",
     description="A package for simulating binary coherent phase field with elasticity.",
     author="Furukawa Tan",
     packages=find_packages(),
